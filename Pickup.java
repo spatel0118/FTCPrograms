e8e9ef12dca9cae74ae20c89466e765440c8e203
colin-zhu <ccolinz2002@gmail.com>
2016-12-21 17:41:12 -0500

    Update Pickup.java

--- Pickup.java
+++ Pickup.java
@@ -1,20 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Pickup {
 
-    
+
 
     private enum pickupEnum {Run, Stop}
     private pickupEnum PickupState = pickupEnum.Stop;
 
 
-        public DcMotor Pickup = null;
-        HardwareMap hwMap = null;
+    public DcMotor Pickup = null;
+    HardwareMap hwMap = null;
 
 
-        public void init(HardwareMap ahwMap) {
+    public void init(HardwareMap ahwMap) {
         //Save reference to Hardware map
         hwMap = ahwMap;
         Pickup = hwMap.dcMotor.get("Pickup");
@@ -28,8 +29,18 @@ public class Pickup {
         this.Pickup = hardwareMap.dcMotor.get(Pickup);
     }
 
+    public void Reverse()
+    {
+        Pickup.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void Forward()
+    {
+        Pickup.setDirection(DcMotor.Direction.FORWARD);
+    }
+
     public void run() {
-        Pickup.setPower(-1.0); PickupState = pickupEnum.Run;
+        Pickup.setPower(1.0); PickupState = pickupEnum.Run;
     }
 
     public void stop() {
@@ -47,5 +58,7 @@ public class Pickup {
         }
     }
 }
+
+
 
 
