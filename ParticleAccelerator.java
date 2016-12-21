55ec49ec250b7df0133538f10dc05984a00c5907
colin-zhu <ccolinz2002@gmail.com>
2016-12-21 17:40:12 -0500

    Update ParticleAccelerator.java

--- ParticleAccelerator.java
+++ ParticleAccelerator.java
@@ -1,39 +1,63 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 /**
  * Created by SaajanPatel on 12/3/16.
  */
 
- class Drivetrain {
-   // public DcMotor Left1 = null;
-    //public DcMotor Left2 = null;
-    public DcMotor Right1 = null;
-    public DcMotor Right2 = null;
+class ParticleAccelerator {
+    private enum acceleratorEnum {Run, Stop}
+    private acceleratorEnum AcceleratorState = acceleratorEnum.Stop;
+
+    public DcMotor ShooterRight = null;
+    public DcMotor ShooterLeft = null;
+    //public DcMotor Accelerator = null;
     HardwareMap hwMap = null;
 
-    public Drivetrain() {
-    }
 
     public void init(HardwareMap ahwMap) {
         //Save reference to Hardware map
         hwMap = ahwMap;
-       // Left1 = hwMap.dcMotor.get("Left1");
-        //Left2 = hwMap.dcMotor.get("Left2");
-        Right1 = hwMap.dcMotor.get("Right1");
-        Right2 = hwMap.dcMotor.get("Right2");
-       // Left1.setDirection(DcMotor.Direction.REVERSE);
-       // Left2.setDirection(DcMotor.Direction.REVERSE);
-        Right1.setDirection(DcMotor.Direction.FORWARD);
-        Right2.setDirection(DcMotor.Direction.FORWARD);
-        //Left1.setPower(0);
-        //Left2.setPower(0);
-        Right1.setPower(0);
-        Right2.setPower(0);
-       // Left1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-       // Left2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        Right1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        Right2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ShooterRight = hwMap.dcMotor.get("ShooterRight");
+        ShooterLeft = hwMap.dcMotor.get("ShooterLeft");
+
+        ShooterLeft.setDirection(DcMotor.Direction.REVERSE);
+        ShooterRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+    public ParticleAccelerator(String ShooterRight, String ShooterLeft, HardwareMap hardwareMap) {
+        this.ShooterRight = hardwareMap.dcMotor.get(ShooterRight);
+        this.ShooterLeft = hardwareMap.dcMotor.get(ShooterLeft);
+    }
+
+
+    public void run() {
+        ShooterLeft.setPower(1.0);
+        ShooterRight.setPower(1.0);
+
+        AcceleratorState = acceleratorEnum.Run;
+    }
+
+    public void stop() {
+        ShooterLeft.setPower(0);
+        ShooterRight.setPower(0);
+        // Accelerator.setPower(0);
+        AcceleratorState = acceleratorEnum.Stop;
+    }
+
+    @Override
+    public String toString() {
+        switch (AcceleratorState){
+            case Run:
+                return "Running";
+            case Stop:
+                return "Stopped";
+            default:
+                return "Unknown";
+        }
     }
 }
+
+
