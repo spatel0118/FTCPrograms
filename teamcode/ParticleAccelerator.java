77c1ca27662599be9e3d1df9982246aaf20733da
spatel0118 <saajan.patel.inbox@gmail.com>
2017-01-07 20:33:31 -0500

    Update ParticleAccelerator.java
    Made Particle Accelerator more consistent. Runs at constant speed. Add encoders to shooter.

--- teamcode/ParticleAccelerator.java
+++ teamcode/ParticleAccelerator.java
@@ -1,13 +1,16 @@
+
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 /**
  * Created by SaajanPatel on 12/3/16.
  */
 
 class ParticleAccelerator {
     private enum acceleratorEnum {Run, Stop}
+
     private acceleratorEnum AcceleratorState = acceleratorEnum.Stop;
 
     public DcMotor ShooterRight = null;
@@ -16,14 +19,29 @@ class ParticleAccelerator {
     HardwareMap hwMap = null;
 
 
+
+    public static final int  SHOOTER_MAX_SPEED    = 4000;
+    public static final double SHOOTER_LEFT_POWER  =  0.136;
+    public static final double SHOOTER_RIGHT_POWER   =  -0.136;
+
+
     public void init(HardwareMap ahwMap) {
         //Save reference to Hardware map
         hwMap = ahwMap;
         ShooterRight = hwMap.dcMotor.get("ShooterRight");
         ShooterLeft = hwMap.dcMotor.get("ShooterLeft");
 
-        ShooterLeft.setDirection(DcMotor.Direction.REVERSE);
+        ShooterLeft.setDirection(DcMotor.Direction.FORWARD);
         ShooterRight.setDirection(DcMotor.Direction.REVERSE);
+
+        ShooterLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ShooterRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        ShooterLeft.setMaxSpeed(SHOOTER_MAX_SPEED);
+        ShooterRight.setMaxSpeed(SHOOTER_MAX_SPEED);
+
+
+
     }
 
 
@@ -32,19 +50,13 @@ class ParticleAccelerator {
         this.ShooterLeft = hardwareMap.dcMotor.get(ShooterLeft);
     }
 
-
     public void run() {
-       //do we need this now? 
-        //ShooterLeft.setPower(1.0);
-       // ShooterRight.setPower(1.0);
-        
-        //sets the speed for the Particle shooter
-        for (float speed = 0; speed <= 1; speed += 0.2) {
-            ShooterLeft.setPower(speed);
-            ShooterRight.setPower(speed);
+        ShooterLeft.setPower(SHOOTER_LEFT_POWER);
+        ShooterRight.setPower(SHOOTER_RIGHT_POWER);
 
-            AcceleratorState = acceleratorEnum.Run;
+        AcceleratorState = acceleratorEnum.Run;
     }
+
 
     public void stop() {
         ShooterLeft.setPower(0);
@@ -62,9 +74,12 @@ class ParticleAccelerator {
                 return "Stopped";
             default:
                 return "Unknown";
+
+
+
         }
-
-
-
     }
 }
+
+
+
